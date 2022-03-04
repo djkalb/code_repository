67a4628db7bdba510debc36c93144dc61d432338
djkalb <71739290+djkalb@users.noreply.github.com>
2022-03-04 14:44:01 -0800

    started version 3

---
+++ code_rep/code_rep_console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ModelLibrary;
+using ModelLibrary.Models;
+
+namespace code_rep_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new RepositoryContext())
+            {
+                var user = new User() {
+                    UserName = "iamme",
+                    
+                    IpAddress = "123456789"
+                };
+
+                ctx.Users.Add(user);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("umm hell?");
+        }
+    }
+}
